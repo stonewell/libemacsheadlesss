f1ec1b0d670e6e223dae3b35c931d252070d4b21
Jingnan Si <jingnan.si@gmail.com>
2022-10-17 21:58:45 -0700

    add server and test cli code

--- src/main.go
+++ src/main.go
@@ -1,16 +1,55 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"net"
+	"google.golang.org/grpc"
 	pb "github.com/stonewell/emacsheadless/proto"
+	log "github.com/sirupsen/logrus"
 )
 
 type server struct {
 	pb.UnimplementedHeadlessServer
 }
 
-
-//test
 func main() {
-	fmt.Println("This is a golang project with cmake.")
+	start_server()
+}
+
+func (*server) Connect(stream pb.Headless_ConnectServer) error {
+	log.Info("Connect Function")
+
+	for {
+		// Receive the request and possible error from the stream object
+		req, err := stream.Recv()
+
+		// If there are no more requests, we return
+		if err == io.EOF {
+			return nil
+		}
+
+		// Handle error from the stream object
+		if err != nil {
+			log.Println("Error when reading client request stream:", err)
+			return err
+		}
+
+		log.Println("recv req:", req.Type)
+	}
+}
+
+func start_server() {
+	log.Info("Starting the server...")
+
+	lis, err := net.Listen("tcp", ":3000")
+	if err != nil {
+		log.Fatalf("Unable to listen on port 3000: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterHeadlessServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 }
---
+++ test/test_client.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	pb "github.com/stonewell/emacsheadless/proto"
+	log "github.com/sirupsen/logrus"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Client Stream...")
+	fmt.Println()
+
+	opts := grpc.WithInsecure()
+	con, err := grpc.Dial("localhost:3000", opts)
+	if err != nil {
+		log.Fatalf("Error connecting: %v \n", err)
+	}
+
+	defer con.Close()
+	c := pb.NewHeadlessClient(con)
+
+	stream, err := c.Connect(context.Background())
+	if err != nil {
+		log.Fatalf("Error when getting stream object: %v", err)
+		return
+	}
+
+	stream.Send(&pb.Cmd {
+		Type: pb.CmdType_DrawChar,
+	})
+
+	time.Sleep(1 * time.Second)
+
+	stream.CloseSend()
+}
